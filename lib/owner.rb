a900dab23b49fe70eab8e713a06768ad74380c1e
Brian Haltiwanger <bhalt@bellsouth.net>
2019-02-06 19:22:07 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,15 @@
 class Owner
-  # code goes here
+  # has many pets
+  attr_accessor :pets
+   
+    def initialize(name, genre)
+      @name = name
+      @genre = genre
+    end
+  end
+
+  
+
+
+  
 end
