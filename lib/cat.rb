cfcdd1c8e26e6db40b3b92dfcb5de1e6eeec2a45
Brian Haltiwanger <bhalt@bellsouth.net>
2019-02-06 19:22:21 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,18 @@
 class Cat
-  # code goes here
+  # belongs to owner
+  attr_accessor :pets
+
+  def initialize(pets)
+    @pets = pets
+    @fishes = []
+    @cats = []
+    @dogs = []
+  end
+end
+
+  def buy_cat
+
+  end
+
+  def buy
 end
