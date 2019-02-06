41dd780315278ffba78b60b3c078ac17dc2fd549
Brian Haltiwanger <bhalt@bellsouth.net>
2019-02-06 19:23:34 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,14 +2,17 @@ class Owner
   # has many pets
   attr_accessor :pets
 
-    def initialize(name, genre)
-      @name = name
-      @genre = genre
-    end
+  def initialize(pets)
+    @pets = pets
+    @fishes = []
+    @cats = []
+    @dogs = []
+  end
+end
+
+  def buy_cat
+
   end
 
-
-
-
-
+  def buy
 end
